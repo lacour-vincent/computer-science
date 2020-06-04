902f4bf38e6015c0010dc1365a9e5e53bb1be1a4
Lacour Vincent <lacour.vincent.app@gmail.com>
2020-06-04 19:51:38 +0200

    Add Linked List

---
+++ src/data-structures/linked-list/linked-list.test.ts
@@ -0,0 +1,125 @@
+import LinkedList from './linked-list';
+
+const createLinkedList = (): LinkedList<number> => {
+	const list = new LinkedList<number>();
+	list.add(1);
+	list.add(2);
+	list.add(3);
+	return list;
+};
+
+describe('LinkedList', () => {
+	describe('length', () => {
+		it('should return the length of an empty linked-list', () => {
+			const list = new LinkedList();
+			expect(list.length).toBe(0);
+		});
+
+		it('should return the length of a linked-list', () => {
+			const list = createLinkedList();
+			expect(list.length).toBe(3);
+		});
+	});
+
+	describe('add', () => {
+		it('should add an element in an empty linked-list', () => {
+			const list = new LinkedList<number>();
+			list.add(1);
+			expect([...list]).toEqual([1]);
+		});
+
+		it('should add an element in a linked-list', () => {
+			const list = createLinkedList();
+			list.add(4);
+			expect([...list]).toEqual([1, 2, 3, 4]);
+		});
+	});
+
+	describe('get', () => {
+		it('should return the value associated to the index', () => {
+			const list = createLinkedList();
+			expect(list.get(0)).toBe(1);
+			expect(list.get(1)).toBe(2);
+			expect(list.get(2)).toBe(3);
+		});
+
+		it('should return undefined with empty list', () => {
+			const list = new LinkedList<number>();
+			expect(list.get(0)).toBe(undefined);
+		});
+
+		it('should return undefined with wrong index', () => {
+			const list = createLinkedList();
+			expect(list.get(-1)).toBe(undefined);
+			expect(list.get(3)).toBe(undefined);
+		});
+	});
+
+	describe('indexOf', () => {
+		it('should return the index associated to the value', () => {
+			const list = createLinkedList();
+			expect(list.indexOf(1)).toBe(0);
+			expect(list.indexOf(2)).toBe(1);
+			expect(list.indexOf(3)).toBe(2);
+		});
+
+		it('should return the index of the first value found', () => {
+			const list = createLinkedList();
+			list.add(1);
+			expect(list.indexOf(1)).toBe(0);
+		});
+
+		it('should return -1 with empty linked-list', () => {
+			const list = new LinkedList<number>();
+			expect(list.indexOf(0)).toBe(-1);
+		});
+
+		it('should return -1 when the value is not found', () => {
+			const list = createLinkedList();
+			expect(list.indexOf(4)).toBe(-1);
+		});
+	});
+
+	describe('remove', () => {
+		it('should remove the head of the linked-list', () => {
+			const list = createLinkedList();
+			list.remove(0);
+			expect([...list]).toEqual([2, 3]);
+		});
+
+		it('should remove the tail of the linked-list', () => {
+			const list = createLinkedList();
+			list.remove(2);
+			expect([...list]).toEqual([1, 2]);
+		});
+
+		it('should remove an element in the linked-list', () => {
+			const list = createLinkedList();
+			list.remove(1);
+			expect([...list]).toEqual([1, 3]);
+		});
+
+		it('should throw RangeError with an empty linked-list', () => {
+			const list = new LinkedList();
+			expect(() => list.remove(0)).toThrow(RangeError);
+		});
+
+		it('should throw RangeError with wrong index', () => {
+			const list = createLinkedList();
+			expect(() => list.remove(-1)).toThrow(RangeError);
+			expect(() => list.remove(3)).toThrow(RangeError);
+		});
+	});
+
+	describe('iterator', () => {
+		it('should return the array representation of an empty linked-list', () => {
+			const list = new LinkedList();
+			expect([...list]).toEqual([]);
+		});
+
+		it('should return the array representation of a linked-list', () => {
+			const list = createLinkedList();
+			expect([...list]).toEqual([1, 2, 3]);
+		});
+	});
+});
---
+++ src/data-structures/linked-list/linked-list.ts
@@ -0,0 +1,90 @@
+interface LinkedListNode<T> {
+	data: T;
+	next: LinkedListNode<T> | null;
+}
+
+class LinkedList<T> {
+	private head: LinkedListNode<T> = null;
+
+	get length(): number {
+		let current = this.head;
+		let i = 0;
+		while (current !== null) {
+			i++;
+			current = current.next;
+		}
+		return i;
+	}
+
+	public add(value: T) {
+		const node: LinkedListNode<T> = { data: value, next: null };
+		let current = this.head;
+		if (current === null) this.head = node;
+		else {
+			while (current.next !== null) current = current.next;
+			current.next = node;
+		}
+	}
+
+	public get(index: number): T | undefined {
+		if (index < 0) return undefined;
+		let current = this.head;
+		let i = 0;
+		while (current !== null && i < index) {
+			i++;
+			current = current.next;
+		}
+		return current?.data;
+	}
+
+	public indexOf(value: T): number {
+		let current = this.head;
+		let index = 0;
+		while (current !== null) {
+			if (current.data === value) return index;
+			index++;
+			current = current.next;
+		}
+		return -1;
+	}
+
+	public remove(index: number): T {
+		let current = this.head;
+		if (current === null || index < 0) throw new RangeError();
+
+		if (index === 0) {
+			const { data } = current;
+			this.head = current.next;
+			return data;
+		}
+
+		let previous: LinkedListNode<T> = null;
+		let i = 0;
+		while (current !== null && i < index) {
+			i++;
+			previous = current;
+			current = current.next;
+		}
+		if (current === null) throw new RangeError();
+		previous.next = current.next;
+		return current.data;
+	}
+
+	public clear() {
+		this.head = null;
+	}
+
+	private *values() {
+		let current = this.head;
+		while (current !== null) {
+			yield current.data;
+			current = current.next;
+		}
+	}
+
+	public [Symbol.iterator]() {
+		return this.values();
+	}
+}
+
+export default LinkedList;
