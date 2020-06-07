dc368980ac2681b272e49ae55090bb4c567ea08e
Lacour Vincent <lacour.vincent.app@gmail.com>
2020-06-07 21:39:56 +0200

    Add Stack

---
+++ src/data-structures/stack/stack.ts
@@ -0,0 +1,43 @@
+class Stack<T> {
+	public data: Array<T> = [];
+
+	get length(): number {
+		return this.data.length;
+	}
+
+	public isEmpty(): boolean {
+		return this.data.length === 0;
+	}
+
+	public push(element: T) {
+		this.data.push(element);
+	}
+
+	public pop(): T | undefined {
+		if (this.isEmpty()) return undefined;
+		return this.data.pop();
+	}
+
+	public peek(): T {
+		if (this.isEmpty()) return undefined;
+		return this.data[this.data.length - 1];
+	}
+
+	public clear() {
+		this.data = [];
+	}
+
+	public *values() {
+		let index = 0;
+		while (this.data[index] !== undefined) {
+			yield this.data[index];
+			index++;
+		}
+	}
+
+	public [Symbol.iterator]() {
+		return this.values();
+	}
+}
+
+export default Stack;
